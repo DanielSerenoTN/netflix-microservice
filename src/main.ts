13ddc417ae18de37aefc3378d7ceca42f7b8dbb5
DanielSerenoTN <daniel.marin@tendril.us>
2023-08-12 09:43:11 -0600

    feat: global data validation configuration using ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(20050);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      enableDebugMessages: true,
+      skipUndefinedProperties: false,
+      skipNullProperties: false,
+      skipMissingProperties: false,
+      disableErrorMessages: false,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      always: true,
+    }),
+  );
+  await app.listen(20060);
 }
 bootstrap();
