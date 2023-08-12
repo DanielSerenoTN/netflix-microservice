236c077b857a16430380daa8eb79a3d150277d6d
DanielSerenoTN <daniel.marin@tendril.us>
2023-08-12 11:08:14 -0600

    feat: swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,14 @@ async function bootstrap() {
       always: true,
     }),
   );
+
+  //swagger
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Netflix Microservice Documentation')
+    .setDescription('Netfliz API description')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(20060);
 }
 bootstrap();
