64bc6c254f56c68c33e151a4dad1e56342a406dc
DanielSerenoTN <daniel.marin@tendril.us>
2023-08-13 12:23:41 -0600

    feat: dtos are created to update the movie and grouped reviews by platform.

---
+++ src/movie/application/model/movie.update.dto.ts
@@ -0,0 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsMongoId,
+} from 'class-validator';
+import MovieDto from './movie.dto';
+
+export default class UpdateMovieDto extends MovieDto {
+  @ApiProperty({
+    description: 'ID of the movie',
+    example: '64d7b6878b53321d0bb152bb',
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  @IsMongoId()
+  id: string;
+
+  @ApiProperty({
+    description: 'Ids of the movienreviews',
+    example: ['64d7d3e06a734de4a6e570b9'],
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  reviews: string[];
+}
