3217c902d08a36954e78362460e353206828c645
DanielSerenoTN <daniel.marin@tendril.us>
2023-08-13 12:53:22 -0600

    feat: import movie, platform, review modules to the main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import PlatformModule from './platform/application/platform.module';
+import ReviewModule from './review/application/review.module';
+import MovieModule from './movie/application/movie.module';
 
 @Module({
-  imports: [PlatformModule],
+  imports: [PlatformModule, ReviewModule, MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
