57d7f6fc3e64b84898c3088fbadb3aaa6fdfb70b
DanielSerenoTN <daniel.marin@tendril.us>
2023-08-13 06:49:54 -0600

    entity restructuring, dto of the film

--- src/movie/application/model/movie.dto.ts
+++ src/movie/application/model/movie.dto.ts
@@ -1,1 +1,96 @@
-export default class MovieDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsOptional,
+} from 'class-validator';
+
+export default class MovieDto {
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Title of the movie',
+    example: 'John Wick',
+    required: true,
+    nullable: false,
+  })
+  title: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    description: 'Transformed title of the movie',
+    example: 'john-wick',
+    required: false,
+    nullable: true,
+  })
+  slug: string;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Image of the movie',
+    example: 'image.jpg',
+    required: true,
+    nullable: false,
+  })
+  image: string;
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Director of the movie',
+    example: 'Guillermo del toro',
+    required: true,
+    nullable: false,
+  })
+  director: string;
+  @IsDefined()
+  @IsNumber()
+  @Min(0)
+  @Max(5)
+  @ApiProperty({
+    description: 'Score of the review (0 to 5)',
+    example: 4.5,
+    required: true,
+    nullable: false,
+  })
+  score: number;
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Budget allocated to the movie',
+    example: '200,000,000',
+    required: true,
+    nullable: false,
+  })
+  budget: string;
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({
+    description: 'Money raised by the movie',
+    example: '600,000,000',
+    required: true,
+    nullable: false,
+  })
+  collection: string;
+  @ApiProperty({
+    description: 'Platforms where the film is available',
+    example: ["64d7d3c56a734de4a6e570b7"],
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  platforms: string[]; 
+
+}
--- src/movie/domain/entity/movie.entity.ts
+++ src/movie/domain/entity/movie.entity.ts
@@ -1,1 +1,149 @@
-export default class MovieEntity {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsDate,
+  IsOptional,
+  ValidateNested,
+  IsArray,
+} from 'class-validator';
+import PlatformEntity from '../../../platform/domain/entity/platform.entity';
+import ReviewEntity from '../../../review/domain/entity/review.entity';
+
+
+export default class MovieEntity {
+  @ApiProperty({
+    description: 'ID of the movie',
+    example: '64d7b6878b53321d0bb152bb',
+    required: true,
+    nullable: false,
+  })
+  id: string;
+
+  @ApiProperty({
+    description: 'Title of the movie',
+    example: "Megalodon 3",
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @ApiProperty({
+    description: 'Slug of the movie',
+    example: "megalodon-3",
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  slug: string;
+
+  @ApiProperty({
+    description: 'Image of the movie',
+    example: 'megalodon.jpg',
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+
+  @ApiProperty({
+    description: 'Director of the movie',
+    example: 'Guillermo del toro',
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  director: string;
+
+  @IsDefined()
+  @IsNumber()
+  @Min(0)
+  @Max(5)
+  score: number;
+
+  @ApiProperty({
+    description: 'Budget assigned to the film',
+    example: '200,000,000',
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  budget: string;
+  @ApiProperty({
+    description: 'Money raised by the movie',
+    example: '600,000,000',
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  collection: string;
+  @ApiProperty({
+    description: 'Ids of the platform being reviewed',
+    example:  [
+        {
+            "id": "64d856f23cf92be6742e747b",
+            "title": "Plataforma 2",
+            "icon": "icono-plataforma-2",
+            "createdAt": "2023-08-13T04:07:14.363Z",
+            "updatedAt": "2023-08-13T06:18:34.743Z",
+            "deletedAt": null,
+            "movieId": "64d875baf430a5a429a5f732"
+        }
+    ],
+    required: true,
+    nullable: false,
+  })
+  @IsDefined()
+  @IsArray()
+  @ValidateNested({ each: true })
+  platforms?: PlatformEntity[];
+  @IsDefined()
+  @IsArray()
+  @ValidateNested({ each: true })
+  reviews?: ReviewEntity[]; 
+
+  @ApiProperty({
+    description: 'Creation timestamp',
+    example: '2023-08-12T16:42:47.093Z',
+    required: true,
+    nullable: false,
+  })
+  @IsDate()
+  createdAt: Date;
+
+  @ApiProperty({
+    description: 'Update timestamp',
+    example: '2023-08-12T16:42:47.093Z',
+    required: true,
+    nullable: false,
+  })
+  @IsDate()
+  updatedAt: Date;
+
+  @ApiProperty({
+    description: 'Delete timestamp',
+    example: '2023-07-12T16:42:47.093Z',
+    nullable: true,
+    required: false,
+  })
+  @IsOptional()
+  @IsDate()
+  deletedAt?: Date | null;
+}
